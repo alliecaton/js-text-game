721fe0dfa1c40975655c3217fd94f9ee1569825e
Allie <alliecaton3@gmail.com>
2021-06-26 13:00:30 -0400

    created a state obj to populate data with

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import { gameData } from "./module.js";
 window.addEventListener("DOMContentLoaded", (event) => {
   if (data[0].pid === "1") {
-    passage.innerText = data[0].text;
+    state = { passage: data[0].text, links: data[0].links };
+    console.log(state);
+    passage.innerText = state.passage;
     console.log(data[0].text);
     button.innerText = data[0].links[0].name;
   }
 });
+
+let state = {
+  passage: "",
+  links: null,
+};
 
 const rawData = gameData();
 const data = rawData.passages;
