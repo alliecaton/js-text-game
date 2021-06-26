fe7d39ef6f0537e9dd88ba3479ff3996e8a006fd
Allie <alliecaton3@gmail.com>
2021-06-26 12:48:43 -0400

    some initial functionality to be refactored

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import { gameData } from "./module.js";
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (data[0].pid === "1") {
+    passage.innerText = data[0].text;
+    console.log(data[0].text);
+    button.innerText = data[0].links[0].name;
+  }
+});
 
 const rawData = gameData();
 const data = rawData.passages;
 
-console.log(data);
+const passage = document.getElementById("passage");
+const button = document.getElementById("first-choice");
